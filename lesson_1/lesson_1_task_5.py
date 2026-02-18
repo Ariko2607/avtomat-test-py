d773e0e3e1cc7b3962ab469cad1b01177989d809
Ariko2607 <anime100398@gmail.com>
2026-02-18 21:58:27 +0300

    avtomat-test-py

---
+++ lesson_1/lesson_1_task_5.py
@@ -0,0 +1,15 @@
+def print_num(num):  #создаём функцию с аргументом num
+    print(num, end="")  #end="" означает, что после печати не будет перехода на новую строку
+
+
+print_num(8)  #вызываем функцию 11 раз что бы получить номер из наедающей рекламы :) - 88005553535
+print_num(8)
+print_num(0)
+print_num(0)
+print_num(5)
+print_num(5)
+print_num(5)
+print_num(3)
+print_num(5)
+print_num(3)
+print_num(5)
